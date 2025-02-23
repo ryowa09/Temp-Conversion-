9f800f2b3f03d59a08991b19f2453c1713f1b46f
Ryan Umlas <168735615+ryowa09@users.noreply.github.com>
2025-02-23 13:19:59 +0800

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,22 @@
+const inputBox = document.getElementById("inputBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelcius = document.getElementById("toCelcius");
+const result = document.getElementById("result");
+let temp;
+
+function tempConvert() {
+    if (toFahrenheit.checked) {
+        temp = Number(inputBox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+    }
+
+    else if (toCelcius.checked) {
+        temp = Number(inputBox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else {
+        result.textContent = "Select a unit!";
+    }
+}
